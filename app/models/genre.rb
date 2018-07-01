dfccca66f3e1c2c5d1f4e4d2e76c27b796a9eff6
Matt Farmer <mrfarmer777@gmail.com>
2018-07-01 20:30:47 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,5 +7,5 @@ class Genre < ActiveRecord::Base
 
   has_many :song_genres #has many song-genre relationships
   has_many :songs, through: :song_genres
-  has_many :artists  #? Not sure about this relationship here. Does it just create an array isntance variable to hold this?
+  has_many :artists, through: :songs #? Not sure about this relationship here. Does it just create an array isntance variable to hold this?
 end
