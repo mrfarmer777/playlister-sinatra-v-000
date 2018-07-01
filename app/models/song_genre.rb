dc322f20cb8917a0b6bac69ad89e94ef463e378c
Matt Farmer <mrfarmer777@gmail.com>
2018-07-01 20:26:45 +0000

    automatically backed up by learn

--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -1,4 +1,10 @@
+require_relative './concerns/slugifiable'
+
+
 class SongGenre<ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+  
   belongs_to :song
   belongs_to :genre
 end
