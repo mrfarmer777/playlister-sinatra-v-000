daeb03023fc0e1e32b62627d826bfae423646681
Matt Farmer <mrfarmer777@gmail.com>
2018-07-01 20:22:26 +0000

    automatically backed up by learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,7 +2,7 @@ require 'pry'
 module Slugifiable
 
   module ClassMethods
-    def find_by_slug(slug)      
+    def find_by_slug(slug)
       res=self.all.find {|item| item.slug==slug}
       res
     end
@@ -10,7 +10,7 @@ module Slugifiable
 
   module InstanceMethods
     def slug
-      self.name.downcase.gsub(" ","-") #???
+      self.name.downcase.gsub(" ","-") 
     end
   end
 
